0d89ef771cb1f752fa07004cb74af6c11c4c224c
Chris Hilsdon <chris@koolserve.uk>
2016-06-09 08:10:18 +0100

    Moved composer update to its own function
    Now commits the changed composer files if bolt was updated

--- updateBolt.sh
+++ updateBolt.sh
@@ -3,11 +3,27 @@
 function getBoltVersion() {
   # Check that bolt has been installed via composer
   if [ ! -f vendor/bin/nut ]; then
-    echo "Nut couldn't be found. Is it installed here?"
+    echo "Nut couldn't be found. Is bolt even installed here?"
     exit;
   fi
 
   boltV=$(php vendor/bin/nut -V --no-ansi | sed "s/.*version //g")
+}
+
+function runComposerUpdate () {
+  composer update --prefer-dist -o
+}
+
+function commitTheChanges() {
+  git add composer.lock extensions/composer.json
+  git commit -m "[Composer] Bolt :arrow_up: v$boltV"
+  git push
+
+  # Check if we can push to prod
+  # http://stackoverflow.com/a/23353971
+  if git remote | grep prod > /dev/null; then
+    git push prod
+  fi
 }
 
 getBoltVersion;
@@ -16,7 +32,7 @@ currentV=$boltV
 
 echo 'Bolt is currently at' $boltV;
 
-composer update --no-interaction;
+runComposerUpdate;
 
 getBoltVersion;
 
@@ -27,4 +43,5 @@ then
   echo Bolt is at the latest version
 else
   echo Bolt has been updated to the latest version, lets commit the composer.lock file
+  commitTheChanges;
 fi
